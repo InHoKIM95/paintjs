8242370a3f007d2252b0f53502602ceabd682b1c
InHoKIM95 <jkl000095@gmail.com>
2020-08-25 10:14:25 +0900

    PaintJS

--- app.js
+++ app.js
@@ -9,130 +9,121 @@ canvas.width = 700;
 canvas.height = 700;
 
 ctx.fillStyle = "white";
-ctx.fillRect(0,0, canvas.width, canvas.height);
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = "black";
 ctx.lineWidth = 15;
 
 let painting = false;
 
-function stopPainting(){
+function stopPainting() {
     painting = false;
 }
 
-function startPainting(){
+function startPainting() {
     painting = true;
 }
 
-function onMouseMove(event){
+function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-    if(!painting){
+    if (!painting) {
         ctx.beginPath();
-        ctx.moveTo(x,y);
+        ctx.moveTo(x, y);
     } else {
-        ctx.lineTo(x,y);
+        ctx.lineTo(x, y);
         ctx.stroke();
     }
 }
 
-function onMouseDown(event){
+function onMouseDown(event) {
     painting = true;
 }
 
-function handleColorClick(event){
+function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
 }
 
-function handleRangeChange(event){
+function handleRangeChange(event) {
     const size = event.target.value;
-    console.log("pensize : ",size);
+    console.log("pensize : ", size);
     ctx.lineWidth = size;
 }
 
-function handleModeClick(){
+function handleModeClick() {
     // ctx.clearRect(0,0, canvas.width, canvas.height);
     ctx.fillStyle = "white";
-    ctx.fillRect(0,0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function handleCM(event){
+function handleCM(event) {
     event.preventDefault();
 }
 
-function handleSaveClick(){
+function handleSaveClick() {
     const dataURL = canvas.toDataURL();
 
-    var imgData = ctx.getImageData(0,0,canvas.width,canvas.height);
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
 
     var data = imgData.data;
 
-    for(var i = 0 ; i < data.length ; i++){
-        if(data[i] != 255){
-            console.log(i);
-        }
-    }
-
-    var a1 = new Array();
-    var a2 = new Array();
-    var cnt = 1;
-    for(var i = 0 ; i < data.length ; i++){
-        if(i%4==0){
+    var a1 = new Array(canvas.width * canvas.height);
+    var a2 = new Array(4);
+    var cnt = 0;
+    var arraycount = 0;
+    for (var i = 0; i < data.length; i++) {
+        if (i % 4 == 0) {
             a2[0] = data[i];
-        }else if(i%4==1){
+            cnt++;
+        } else if (i % 4 == 1) {
             a2[1] = data[i];
             cnt++;
-        }else if(i%4==2){
+        } else if (i % 4 == 2) {
             a2[2] = data[i];
             cnt++;
-        }else{
+        } else {
             a2[3] = data[i];
             cnt++;
         }
-        if(cnt==4){
-            a1.push(a2);
-            cnt = 1;
+        if (cnt == 4) {
+            a1[arraycount] = a2;
+            arraycount++;
+            cnt = 0;
         }
     }
+    //test ------------------
+    for(var i = 0 ; i < 100 ; i++){
+        console.log(a1[i][0]);
+    }
 
-    var rgbData = Array.from(Array(canvas.width),() => new Array(canvas.height));
+    // var whitecount = 0;
+    // for (var i = 0; i < a1.length; i++) {
+    //     if (a1[i][0] == 255) {
+    //         whitecount++
+    //     }
+    // }
+    // console.log(whitecount);
+
+
+    var rgbData = Array.from(Array(canvas.width), () => new Array(canvas.height));
 
     var count = 0;
-    for(var i = 0; i < rgbData.length ; i++){
-        for(var j = 0; j < rgbData[i].length ; j++){
+    for (var i = 0; i < rgbData.length; i++) {
+        for (var j = 0; j < rgbData[i].length; j++) {
             rgbData[i][j] = a1[count];
             count++;
         }
     }
-
-    for(var i = 0; i < rgbData.length; i++){
-        for(var j = 0 ; j < rgbData[i].length ; j++){
-            if(rgbData[i][j][0] != 255){
-                console.log(i,j);
-            }
-        }
-    }
-
-    console.log(rgbData);
+    // console.log(rgbData[0][0][0]);
+    // console.log(rgbData[0][300][0]);
+    // console.log(rgbData[0][400][0]);
+    // console.log(rgbData[0][500][0]);
 
 
 
-    // var imgData = ctx.getImageData(0,0,canvas.width, canvas.height);
-    // console.log(imgData);
-    // ctx.putImageData(imgData,10,70);
 
-    // $.ajax({
-    //     type:'post',
-    //     url:'image.php',
-    //     data: {'imgBase64' : dataURL},
-    //     success: function(){
-    //         console.log('성공');
-    //     },
-    //     error: function(){
-    //         alert('실패');
-    //     }
-    // });
+
 
 
     // a태그 생성해서 다운로드 받는것
@@ -142,7 +133,7 @@ function handleSaveClick(){
     // link.click();
 }
 
-if(canvas){
+if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
@@ -150,19 +141,19 @@ if(canvas){
     canvas.addEventListener("contextmenu", handleCM);
 }
 
-Array.from(colors).forEach(color => 
+Array.from(colors).forEach(color =>
     color.addEventListener("click", handleColorClick)
 );
 
 
-if(range){
+if (range) {
     range.addEventListener("input", handleRangeChange);
 }
 
-if(mode){
+if (mode) {
     mode.addEventListener("click", handleModeClick);
 }
 
-if(save){
+if (save) {
     save.addEventListener("click", handleSaveClick);
 }
